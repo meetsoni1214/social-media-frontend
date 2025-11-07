5cc69be180d0f9fda1e548173de0212d6fb0b2cc
Meet Soni <meet@incubyte.co>
2025-11-07 23:22:49 +0530

    feat: add utility functions for converting between snake_case and camelCase for object keys

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,55 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function snakeToCamel(str: string): string {
+  return str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+}
+
+export function camelToSnake(str: string): string {
+  return str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+}
+
+export function keysToCamel<T = any>(obj: any): T {
+  if (obj === null || obj === undefined) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(item => keysToCamel(item)) as T;
+  }
+
+  if (typeof obj === 'object' && obj.constructor === Object) {
+    return Object.keys(obj).reduce((acc, key) => {
+      const camelKey = snakeToCamel(key);
+      acc[camelKey] = keysToCamel(obj[key]);
+      return acc;
+    }, {} as any) as T;
+  }
+
+  return obj;
+}
+
+export function keysToSnake<T = any>(obj: any): T {
+  if (obj === null || obj === undefined) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(item => keysToSnake(item)) as T;
+  }
+
+  if (typeof obj === 'object' && obj.constructor === Object) {
+    return Object.keys(obj).reduce((acc, key) => {
+      const snakeKey = camelToSnake(key);
+      acc[snakeKey] = keysToSnake(obj[key]);
+      return acc;
+    }, {} as any) as T;
+  }
+
+  return obj;
 }
